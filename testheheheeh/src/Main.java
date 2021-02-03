9fe55e48087dd7ecc3541c19561e93e28763cbeb
Yanyong1 <1429215258@qq.com>
2021-02-03 20:59:10 +0800

    本地

--- testheheheeh/src/Main.java
+++ testheheheeh/src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+
+
+    }
+    public  void hehei(String[] args) {
+        System.out.println("wowowowowowoow");
     }
 }
